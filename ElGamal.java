aa15a502e89350eb2c79010f20f6e5676e42aab0
JStepnicki <j.stepnicki2002@gmail.com>
2023-04-28 18:26:50 +0200

    added sign () function

--- ElGamal.java
+++ ElGamal.java
@@ -1,4 +1,6 @@
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -10,28 +12,44 @@ public class ElGamal {
     BigInteger h; // public keys
     BigInteger a; // private key
     BigInteger s1;
+    BigInteger s2;
     BigInteger rPrim;
+    BigInteger pSubstractOne;
     private final int keyLength = 2048;
+    private final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
     private final Random random = new SecureRandom();
 
+    public ElGamal() throws NoSuchAlgorithmException {
+    }
+
     public void generateKeys() {
-        p = BigInteger.probablePrime(keyLength+1,random);
+        p = BigInteger.probablePrime(keyLength + 1, random);
         g = new BigInteger(keyLength, random);
         a = new BigInteger(keyLength, random); // to będzie klucz prywatny
-        h = g.modPow(a,p); // (g^a)%p tylko na BigIntach
-        BigInteger pSubstractOne = p.subtract(BigInteger.ONE);
+        h = g.modPow(a, p); // (g^a)%p tylko na BigIntach
+        pSubstractOne = p.subtract(BigInteger.ONE);
+    }
 
-// podobno generuje tu juz podpis
+
+    public BigInteger[] sign(byte[] document) {
+        messageDigest.update(document);
+        BigInteger signature = new BigInteger(1, messageDigest.digest());
         r = new BigInteger(keyLength, random);
-        s1 = g.modPow(r,p);// (g^r)%p tylko na BigIntach
-        while(true) {
-            if(r.gcd(pSubstractOne).equals(BigInteger.ONE))//sprawdzamy czy NWD(r,p-1) = 1 jezlie nie losujemy inne r
+        s1 = g.modPow(r, p);// (g^r)%p tylko na BigIntach
+        while (true) {
+            if (r.gcd(pSubstractOne).equals(BigInteger.ONE))//sprawdzamy czy NWD(r,p-1) = 1 jezlie nie losujemy inne r
                 break;
             else
                 r = r.nextProbablePrime();
         }
         rPrim = r.modInverse(pSubstractOne);//(r^-1)%(p-1) ;D
-
+        BigInteger temp = signature.subtract((a.multiply(s1)));// signature - a * s1
+        s2 = temp.multiply(rPrim.mod(pSubstractOne)); // temp * rPrim%(p-1)
+        BigInteger[] result = new BigInteger[2];
+        result[0] = s1;
+        result[1] = s2;
+        return result;
     }
+
 
 }
