4750ea986a1b4434c502fd47ba9b53f0fa6fd58c
JStepnicki <j.stepnicki2002@gmail.com>
2023-04-28 18:43:32 +0200

    verify() is complted

--- ElGamal.java
+++ ElGamal.java
@@ -11,8 +11,6 @@ public class ElGamal {
     BigInteger r; // public keys
     BigInteger h; // public keys
     BigInteger a; // private key
-    BigInteger s1;
-    BigInteger s2;
     BigInteger rPrim;
     BigInteger pSubstractOne;
     private final int keyLength = 2048;
@@ -35,7 +33,7 @@ public class ElGamal {
         messageDigest.update(document);
         BigInteger signature = new BigInteger(1, messageDigest.digest());
         r = new BigInteger(keyLength, random);
-        s1 = g.modPow(r, p);// (g^r)%p tylko na BigIntach
+        BigInteger s1 = g.modPow(r, p);// (g^r)%p tylko na BigIntach
         while (true) {
             if (r.gcd(pSubstractOne).equals(BigInteger.ONE))//sprawdzamy czy NWD(r,p-1) = 1 jezlie nie losujemy inne r
                 break;
@@ -44,11 +42,19 @@ public class ElGamal {
         }
         rPrim = r.modInverse(pSubstractOne);//(r^-1)%(p-1) ;D
         BigInteger temp = signature.subtract((a.multiply(s1)));// signature - a * s1
-        s2 = temp.multiply(rPrim.mod(pSubstractOne)); // temp * rPrim%(p-1)
+        BigInteger s2 = temp.multiply(rPrim.mod(pSubstractOne)); // temp * rPrim%(p-1)
         BigInteger[] result = new BigInteger[2];
         result[0] = s1;
         result[1] = s2;
         return result;
+    }
+
+    public boolean verify(byte[] document, BigInteger[] signature) {
+        messageDigest.update(document);
+        BigInteger hash = new BigInteger(1, messageDigest.digest());
+        BigInteger result1 = g.modPow(hash, p);
+        BigInteger result2 = h.modPow(signature[0], p).multiply(signature[0].modPow(signature[1], p)).mod(p);
+        return result1.compareTo(result2) == 0;
     }
 
 
