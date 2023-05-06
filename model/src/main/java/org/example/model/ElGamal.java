6c8a92af81a87dab1f31226653c2f7f1866ed5a0
JStepnicki <j.stepnicki2002@gmail.com>
2023-05-06 13:51:21 +0200

    almost done

--- model/src/main/java/org/example/model/ElGamal.java
+++ model/src/main/java/org/example/model/ElGamal.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class ElGamal {
 
-    BigInteger p; // zazwyczaj to N ale w wykaldzie bylo jako "p"
+    BigInteger N;
     BigInteger g; // public keys
     BigInteger r; // public keys
     BigInteger h; // public keys
@@ -23,22 +23,21 @@ public class ElGamal {
     }
 
     public void generateKeys() {
-        p = BigInteger.probablePrime(keyLength + 1, random);
+        N = BigInteger.probablePrime(keyLength + 1, random);
         g = new BigInteger(keyLength, random);
         a = new BigInteger(keyLength, random); // to będzie klucz prywatny
-        h = g.modPow(a, p); // (g^a)%p tylko na BigIntach
-        pSubstractOne = p.subtract(BigInteger.ONE);
+        h = g.modPow(a, N); // (g^a)%N tylko na BigIntach
+        pSubstractOne = N.subtract(BigInteger.ONE);
     }
 
 
     public BigInteger[] sign(byte[] document) {
         messageDigest.update(document);
         BigInteger DocumentHash = new BigInteger(1, messageDigest.digest());
-        r = new BigInteger(keyLength, random);
-        BigInteger s1 = g.modPow(r, p);// (g^r)%p tylko na BigIntach
-        rPrim = r.modInverse(pSubstractOne);//(r^-1)%(p-1) ;D
-        BigInteger temp = DocumentHash.subtract((a.multiply(s1)));// DocumentHash - a * s1
-        BigInteger s2 = temp.multiply(rPrim.mod(pSubstractOne)); // temp * rPrim%(p-1)
+        r = BigInteger.probablePrime(keyLength, random);
+        BigInteger s1 = g.modPow(r, N);// (g^r)%p tylko na BigIntach
+        rPrim = r.modInverse(pSubstractOne);//(r^-1)%(N-1) ;D
+        BigInteger s2 = DocumentHash.subtract(a.multiply(s1)).multiply(rPrim).mod(pSubstractOne);
         BigInteger[] result = new BigInteger[2];
         result[0] = s1;
         result[1] = s2;
@@ -48,21 +47,17 @@ public class ElGamal {
     public boolean verify(byte[] document, BigInteger[] signature) {
         messageDigest.update(document);
         BigInteger DocumentHash = new BigInteger(1, messageDigest.digest());
-        BigInteger result1 = g.modPow(DocumentHash, p); // g^hash %p
-        BigInteger result2 = h.modPow(signature[0], p).multiply(signature[0].modPow(signature[1], p)).mod(p); //h^s1 * s1^s2 %p
+        BigInteger result1 = g.modPow(DocumentHash, N); // g^hash %p
+        BigInteger result2 = h.modPow(signature[0], N).multiply(signature[0].modPow(signature[1], N)).mod(N); //h^s1 * s1^s2 %N
         return result1.compareTo(result2) == 0;
     }
 
-    public BigInteger getP() {
-        return p;
+    public BigInteger getN() {
+        return N;
     }
 
     public BigInteger getG() {
         return g;
-    }
-
-    public BigInteger getR() {
-        return r;
     }
 
     public BigInteger getH() {
@@ -73,7 +68,19 @@ public class ElGamal {
         return a;
     }
 
-    public BigInteger getrPrim() {
-        return rPrim;
+    public void setN(BigInteger n) {
+        N = n;
+    }
+
+    public void setG(BigInteger g) {
+        this.g = g;
+    }
+
+    public void setH(BigInteger h) {
+        this.h = h;
+    }
+
+    public void setA(BigInteger a) {
+        this.a = a;
     }
 }
--- view/src/main/java/com/example/view/ApplicationController.java
+++ view/src/main/java/com/example/view/ApplicationController.java
@@ -1,7 +1,8 @@
 package com.example.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -10,13 +11,13 @@ import org.example.model.ElGamal;
 import org.example.model.Utils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.math.BigInteger;
+import java.nio.file.Files;
 import java.security.NoSuchAlgorithmException;
-import javafx.scene.control.*;
+import java.util.Arrays;
 
 
 public class ApplicationController extends Window {
@@ -24,12 +25,13 @@ public class ApplicationController extends Window {
     public TextField keyH = new TextField();
     public TextField modN = new TextField();
     public TextField keyA = new TextField();
-
     public TextArea signatureArea = new TextArea();
     public TextArea publicText = new TextArea();
-    private final ElGamal elgamal = new ElGamal();
+
+
+    private ElGamal elgamal = new ElGamal();
     final FileChooser fileChooser = new FileChooser();
-    public Label fileWithPublicText = new Label();
+
 
     public ApplicationController() throws NoSuchAlgorithmException {
     }
@@ -40,19 +42,21 @@ public class ApplicationController extends Window {
         elgamal.generateKeys();
         keyG.setText(elgamal.getG().toString(16));
         keyH.setText(elgamal.getH().toString(16));
-        modN.setText(elgamal.getP().toString(16));
+        modN.setText(elgamal.getN().toString(16));
         keyA.setText(elgamal.getA().toString(16));
 
     }
+
     @FXML
-    protected void wczytajKlucze(){
-    }
-    @FXML
-    protected void zapiszKlucze(){
+    protected void wczytajKlucze() {
     }
 
     @FXML
-    protected void weryfikujPodpisTekstu(){
+    protected void zapiszKlucze() {
+    }
+
+    @FXML
+    protected void weryfikujPodpisTekstu() {
         BigInteger[] signature = Arrays.stream(signatureArea.getText()
                         .split("\n"))
                 .map(Utils::hexToBytes)
@@ -72,28 +76,31 @@ public class ApplicationController extends Window {
     }
 
     @FXML
-    protected void podpiszTekst(){
+    protected void podpiszTekst() {
         BigInteger[] result = elgamal.sign(publicText.getText().getBytes());
-        signatureArea.setText(result[0].toString(16) + "\n" +  result[1].toString(16));
+        signatureArea.setText(result[0].toString(16) + "\n" + result[1].toString(16));
     }
-    @FXML
-    protected void podpiszPlik(){
-        try {
-            if(keyA.getText().equals("") || keyG.getText().equals("") || keyH.getText().equals("") || modN.getText().equals("")) {
-                elgamal.generateKeys();
-            }
-            fileChooser.setTitle("Wybierz plik do podpisania");
-            File fileToSign = fileChooser.showOpenDialog(this);
-            BigInteger[] signNumbers = elgamal.sign(Files.readAllBytes(fileToSign.getAbsoluteFile().toPath()));
-            fileChooser.setTitle("Zapisz podpis do pliku");
-            File signFile = fileChooser.showSaveDialog(this);
-            fileWithPublicText.setText("Otworzono do podpisu: " + fileToSign.getName());
-            try (FileWriter fos = new FileWriter(signFile, true)) {
-                fos.write(signNumbers[0].toString(16));
-                fos.write('\n');
-                fos.write(signNumbers[1].toString(16));
-            }
 
+    @FXML
+    protected void podpiszPlik() {
+        try {
+            if (keyA.getText().equals("") || keyG.getText().equals("") || keyH.getText().equals("") || modN.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.NONE, "NIE WYGENEROWANO KLUCZY !", ButtonType.OK);
+                alert.setTitle("ERROR");
+                alert.showAndWait();
+            } else {
+                fileChooser.setTitle("Wybierz plik do podpisania");
+                File fileToSign = fileChooser.showOpenDialog(this);
+                BigInteger[] signNumbers = elgamal.sign(Files.readAllBytes(fileToSign.getAbsoluteFile().toPath()));
+                fileChooser.setTitle("Zapisz podpis do pliku");
+                File signFile = fileChooser.showSaveDialog(this);
+                try (FileWriter fos = new FileWriter(signFile, true)) {
+                    fos.write(signNumbers[0].toString(16));
+                    fos.write('\n');
+                    fos.write(signNumbers[1].toString(16));
+                }
+
+            }
         } catch (NullPointerException | IOException e) {
             Alert alert = new Alert(Alert.AlertType.NONE, "Nie wybrano pliku!", ButtonType.OK);
             alert.setTitle("Błąd");
@@ -101,7 +108,43 @@ public class ApplicationController extends Window {
             alert.showAndWait();
         }
     }
+
     @FXML
-    protected void weryifkujPlik(){
+    protected void weryifkujPlik() {
+        try {
+            if (keyA.getText().equals("") || keyG.getText().equals("") || keyH.getText().equals("") || modN.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.NONE, "NIE WYGENEROWANO KLUCZY !", ButtonType.OK);
+                alert.setTitle("ERROR");
+                alert.showAndWait();
+            } else {
+                fileChooser.setTitle("Wybierz plik, który był podpisany");
+                File signedFile = fileChooser.showOpenDialog(this);
+                fileChooser.setTitle("Wybierz plik z podpisem");
+                File fileWithSign = fileChooser.showOpenDialog(this);
+                BigInteger[] signature = Arrays.stream(new String(Files.readAllBytes(fileWithSign.toPath()))
+                                .split("\n"))
+                        .map(Utils::hexToBytes)
+                        .map(BigInteger::new)
+                        .toArray(BigInteger[]::new);
+                if (elgamal.verify(Files.readAllBytes(signedFile.getAbsoluteFile().toPath()), signature)) {
+                    Alert alert = new Alert(Alert.AlertType.NONE, "Zweryfikowano poprawnie", ButtonType.OK);
+                    alert.setTitle("Weryfikacja");
+                    alert.setResizable(false);
+                    alert.showAndWait();
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.NONE, "Zweryfikowano niepoprawnie", ButtonType.OK);
+                    alert.setTitle("Weryfikacja");
+                    alert.setResizable(false);
+                    alert.showAndWait();
+                }
+            }
+        } catch (NullPointerException | FileNotFoundException e) {
+            Alert alert = new Alert(Alert.AlertType.NONE, "Nie wybrano pliku!", ButtonType.OK);
+            alert.setTitle("Błąd");
+            alert.setResizable(false);
+            alert.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
